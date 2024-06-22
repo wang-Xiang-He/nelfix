874231c726df630772d72f3b9327b940214a4392
wang-Xiang-He <55984932+wang-Xiang-He@users.noreply.github.com>
2024-06-22 10:57:55 +0800

    Update nef.py

--- nef.py
+++ nef.py
@@ -31,7 +31,7 @@ async def startup_event():
 async def redirect_to_link():
     # 帳戶信息
     username = "cming0236@gmail.com"
-    password = "ppxm nify pipb ypne"
+    password = "ziei mzdx wdnl rlbz"
 
     # 連接到IMAP服務器
     mail = imaplib.IMAP4_SSL("imap.gmail.com")
